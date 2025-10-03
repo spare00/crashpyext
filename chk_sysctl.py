b7c09a7f28f11c5311b32b2136a7d40e188337ce
Brian Kim <gitspare00@gmail.com>
2025-10-03 11:46:06 +1000

    [chk_sysctl] Enforce the feature

--- chk_sysctl.py
+++ chk_sysctl.py
@@ -1,17 +1,27 @@
 from LinuxDump import sysctl
 import fnmatch
 import sys
+import argparse
 
-def chk_sysctl(patterns, verbose=0):
+def chk_sysctl(patterns, verbose=0, substring=False):
     """
-    Print sysctl tunables matching wildcard pattern(s).
+    Print sysctl tunables matching given pattern(s).
+    By default, fnmatch globbing is used.
+    If substring=True, patterns are treated as simple substrings.
     """
     ctbl = sysctl.getCtlTables()   # dict: name -> ctl_table
     names = sorted(ctbl.keys())
 
     found = False
     for pat in patterns:
-        matches = [n for n in names if fnmatch.fnmatch(n, pat)]
+        if substring:
+            matches = [n for n in names if pat in n]
+        else:
+            # if user didn’t give wildcards, auto-wrap with *
+            if "*" not in pat and "?" not in pat:
+                pat = f"*{pat}*"
+            matches = [n for n in names if fnmatch.fnmatch(n, pat)]
+
         if not matches:
             print(f"No sysctl entries matched: {pat}")
             continue
@@ -34,8 +44,24 @@ def chk_sysctl(patterns, verbose=0):
     if not found:
         print("No matching sysctl tunables.")
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: chk_sym <pattern1> [pattern2] ...")
-    else:
-        chk_sysctl(sys.argv[1:], verbose=1)
+    parser = argparse.ArgumentParser(
+        description="Search sysctl tunables in vmcore via PyKdump"
+    )
+    parser.add_argument(
+        "patterns", nargs="+",
+        help="Patterns to search (globs by default, substring if --substr)"
+    )
+    parser.add_argument(
+        "-s", "--substr", action="store_true",
+        help="Enable substring search mode instead of globbing"
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="count", default=0,
+        help="Increase verbosity (-v, -vv for more)"
+    )
+
+    args = parser.parse_args()
+
+    chk_sysctl(args.patterns, verbose=args.verbose, substring=args.substr)
